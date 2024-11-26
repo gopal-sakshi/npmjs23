efbce992312bd34758923b24f7db522598e15242
gopal612 <gopal612@gmail.com>
2024-11-26 22:34:00 +0530

    using gopal612-js-lib

---
+++ packages23/gopal612-js-lib.js
@@ -0,0 +1,5 @@
+const blah = require("gopal612-js-lib");
+
+console.log("primes ===> ", blah.primes23.printPrimes(1,12));
+console.log("random23 ===> ", blah.primes23.generateRandomNumber());
+console.log("multiply23 ===> ", blah.maths23.multiply(-3,-7));
